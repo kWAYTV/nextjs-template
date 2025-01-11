b6111d0fc7c8a03b19f0bcd0b83f1dec97e28887
kWAY <55437151+kWAYTV@users.noreply.github.com>
2025-01-11 08:21:57 +0100

    Update hero.tsx

--- src/components/core/app/home/hero.tsx
+++ src/components/core/app/home/hero.tsx
@@ -4,7 +4,7 @@ export default function Hero() {
       <h1 className='text-4xl font-bold'>Welcome to NextJS Template</h1>
       <p className='mt-4 text-muted-foreground'>
         Get started by editing{' '}
-        <code className='font-mono'>src/app/page.tsx</code>
+        <code className='font-mono'>src/app/(home)/page.tsx</code>
       </p>
     </div>
   );
